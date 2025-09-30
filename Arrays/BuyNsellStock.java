d5e4bef76484c2464a28f711c29adf495fc9c778
Ayesha Khan <ayeshakhan61996@gmail.com>
2025-09-30 07:52:30 +0530

    Buy And Sell Stock

---
+++ Arrays/BuyNsellStock.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class BuyNsellStock {
+
+    public static int buyAndSellStocks(int prices[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for(int i=0 ; i<prices.length ; i++) {
+            if( buyPrice < prices[i]) {              // profit
+               int profit = prices[i] - buyPrice;    // today's profit = Sp - Bp
+               maxProfit = Math.max(maxProfit , profit);   // sare din ka profit
+            } else {
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+   
+    public static void main(String args[]) {
+       int prices[] = { 7, 1, 5, 3, 6, 4 };
+       System.out.println(buyAndSellStocks(prices));
+    }
+}
