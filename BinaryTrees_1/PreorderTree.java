70978774ba3f5531155b0117d27fb6ed61e3e378
Ayesha Khan <ayeshakhan61996@gmail.com>
2025-10-07 08:28:10 +0530

    Build Preorder Tree

---
+++ BinaryTrees_1/PreorderTree.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class PreorderTree {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        // constructor
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        static int idx = -1;        // static bcoz idx should update at each level
+        
+        public static Node buildTree(int nodes[]) {
+            idx++;
+
+            if(idx >= nodes.length) {
+                return null;
+            }
+            
+            if(nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+    }
+
+    public static void main(String args[]) {
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, 6, -1, -1};
+        BinaryTree tree = new BinaryTree();
+        Node root = tree.buildTree(nodes);
+        System.out.println(root.data);
+    
+    }
+}
+
+// Time Complexity = O(n)
