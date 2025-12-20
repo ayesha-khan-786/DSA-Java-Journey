bebd0acd3352de94221b99da28a2be8d3b378361
Ayesha Khan <ayeshakhan61996@gmail.com>
2025-12-20 18:44:42 +0530

    Sorted Array

---
+++ Recursion/SortedArray.java
@@ -0,0 +1,19 @@
+public class SortedArray {
+
+    public static boolean isSorted(int arr[] , int i) {
+        if( i == arr.length-1) {
+            return true;
+        }
+      
+        if(arr[i] > arr[i+1]) {
+            return false;
+        }
+
+        return isSorted(arr , i+1);     // if above statement is true
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {1, 2, 5, 4};
+        System.out.println(isSorted(arr, 0)); 
+    }
+}
