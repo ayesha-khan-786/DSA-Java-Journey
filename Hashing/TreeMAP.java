2083c6ae0444ea516076c0b9593bab17f87be849
Ayesha Khan <ayeshakhan61996@gmail.com>
2025-12-21 09:00:56 +0530

    Tree MAp

---
+++ Hashing/TreeMAP.java
@@ -0,0 +1,15 @@
+// TreeMap -> keys are sorted
+import java.util.*;
+
+public class TreeMAP {
+
+    public static void main(String args[]) {
+        TreeMap<String, Integer> tm = new TreeMap<>();
+            tm.put("India", 100);
+            tm.put("China", 150);
+            tm.put("US", 50);
+
+            System.out.println(tm);
+
+    }
+}
