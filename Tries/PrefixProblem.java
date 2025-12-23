2b004ae92006c15dc4a1e793f1c757358d0a0d88
Ayesha Khan <ayeshakhan61996@gmail.com>
2025-12-23 17:42:41 +0530

    Prefix for Each Word

---
+++ Tries/PrefixProblem.java
@@ -0,0 +1,63 @@
+public class PrefixProblem {
+
+    static class Node {
+        Node[] children = new Node[26];
+        boolean eow = false;
+        int freq;
+
+        public Node() {
+            for(int i=0 ; i<children.length ; i++) {
+                children[i] = null;
+            }
+            freq = 1;
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word) {
+        Node curr = root;
+        for(int i=0 ; i<word.length() ; i++) {
+            int idx = word.charAt(i)-'a';
+            if(curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            } else {
+                curr.children[idx].freq++;
+            }
+
+            curr = curr.children[idx];
+        }
+
+        curr.eow = true;
+    }
+
+    public static void findPrefix(Node root, String ans) {
+        if(root == null) {
+            return;
+        }
+
+        if(root.freq == 1) {
+            System.out.println(ans);
+            return;
+        }
+
+        for(int i=0 ; i<root.children.length ; i++) {
+            if(root.children[i] != null) {
+                findPrefix(root.children[i], ans+(char)(i+'a'));     // ans - if i=0 add in ans=a, i=1 ans=b
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+
+        String arr[] = {"zebra", "dog", "duck", "dove"};
+        for(int i=0 ; i<arr.length ; i++) {
+            insert(arr[i]);
+        }
+
+        root.freq = -1;         // bcoz above we already init freq = 1, while creating node
+        findPrefix(root, "");
+    }
+}
+
+// TC = O(L) -> L is level in trie
