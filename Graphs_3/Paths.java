ba956a842dccbf66aad8f29f43c57756096ea010
Ayesha Khan <ayeshakhan61996@gmail.com>
2026-01-03 15:48:27 +0530

    Paths from Src to Dest

---
+++ Graphs_3/Paths.java
@@ -0,0 +1,57 @@
+// All Paths from Source to Target
+import java.util.*;
+
+public class Paths {
+
+    static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    static void CreateGraph(ArrayList<Edge> graph[]) {
+        for(int i=0 ; i<graph.length ; i++) {
+            graph[i] = new ArrayList<>();               
+        }
+        graph[0].add(new Edge(0, 3));
+
+        graph[2].add(new Edge(2, 3));
+        
+        graph[3].add(new Edge(3, 1));
+
+        graph[4].add(new Edge(4, 0));
+        graph[4].add(new Edge(4, 1));
+
+        graph[5].add(new Edge(5, 0));
+        graph[5].add(new Edge(5, 2));
+    }
+
+    public static void printAllPath(ArrayList<Edge> graph[], int src, int dest, String path) {
+        if(src == dest) {
+            System.out.println(path+dest);
+            return;
+        }
+
+        // if src == not dest, then call neighbour
+        for(int i=0 ; i<graph[src].size() ; i++) {
+            Edge e = graph[src].get(i);
+            printAllPath(graph, e.dest, dest, path+src);
+        }
+    }
+
+    public static void main(String args[]) {
+    
+        int V = 6;
+        ArrayList<Edge> graph[] = new ArrayList[V];    
+
+        CreateGraph(graph);
+    
+        int src = 5, dest = 1;
+        printAllPath(graph, src, dest, "");     // 5 0 3 1  & 5 2 3 1
+    }
+}
+
