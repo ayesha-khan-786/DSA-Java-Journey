e9174c9d126d8b0cf8254dfafa40c21757c747ea
Ayesha Khan <ayeshakhan61996@gmail.com>
2026-01-14 19:02:03 +0530

    Climbing stairs (Recursion)

---
+++ DP_1/Climbing_stairs.java
@@ -0,0 +1,20 @@
+// Climbing stairs using Recursion
+
+public class Climbing_stairs {
+
+    public static int countWays(int n) {
+        if(n == 0) {
+            return 1;
+        }
+        if(n < 0) {
+            return 0;
+        }
+
+        return countWays(n-1) + countWays(n-2);
+    }
+
+    public static void main(String args[]) {
+        int n = 5;      // n=3 -> 3 ways & n=4 -> 5 ways => Ans -> 8
+        System.out.println(countWays(n));
+    }
+}
