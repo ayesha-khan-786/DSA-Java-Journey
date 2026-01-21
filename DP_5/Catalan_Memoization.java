4f4d5371f914cd3a40afd1b83c8c89312d43014d
Ayesha Khan <ayeshakhan61996@gmail.com>
2026-01-21 08:09:54 +0530

    Catalan's no using Memoization

---
+++ DP_5/Catalan_Memoization.java
@@ -0,0 +1,31 @@
+// Catalan's Number using Recursion
+import java.util.*;
+
+public class Catalan_Memoization {
+
+    public static int catalanMem(int n, int dp[]) {
+        if(n == 0 || n == 1) {
+            return 1;
+        }
+
+        if(dp[n] != -1) {
+            return dp[n];
+        }
+        
+        int ans = 0;       
+        for(int i=0 ; i<=n-1 ; i++) {
+            ans += catalanMem(i, dp) * catalanMem(n-i-1, dp);
+        }
+
+        return dp[n] = ans;
+    }
+
+    public static void main(String args[]) {
+        int n = 4;
+        int dp[] = new int[n+1];
+
+        Arrays.fill(dp, -1);        // Initialize all idx of array with -1
+
+        System.out.println(catalanMem(n, dp));      // 14
+    }
+}
